ba0750a8ac4717cb108b80f6d65f71db81794a3e
El-Moatasem <el.moatasem.mohamed@gmail.com>
2025-02-20 17:10:06 +0200

    Adding Project Files

---
+++ Equity-Bank-Assessment/Pods/Target Support Files/Pods-Equity-Bank-Assessment-Equity-Bank-AssessmentUITests/Pods-Equity-Bank-Assessment-Equity-Bank-AssessmentUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Equity_Bank_Assessment_Equity_Bank_AssessmentUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Equity_Bank_Assessment_Equity_Bank_AssessmentUITests
+@end
---
+++ Equity-Bank-Assessment/Pods/Target Support Files/Pods-Equity-Bank-Assessment/Pods-Equity-Bank-Assessment-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Equity_Bank_Assessment : NSObject
+@end
+@implementation PodsDummy_Pods_Equity_Bank_Assessment
+@end
